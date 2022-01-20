85d88135b7351e41784c0424ac457d6c66c7fca3
Gabriel Inacio <inaciogs03@gmail.com>
2022-01-20 20:36:37 -0300

    primeiro commit

---
+++ Classes/Serie.cs
@@ -0,0 +1,7 @@
+namespace DIO.Series.Classes
+{
+    public class Serie : EntidadeBase
+    {
+        public string Titulo { get; set; }
+    }
+}
---
+++ Enum/Genero.cs
@@ -0,0 +1,16 @@
+namespace DIO.Series.Enum
+{
+    public enum Genero
+    {
+        Acao = 1,
+        Aventura = 2,
+        Comedia = 3,
+        Documentario = 4,
+        Drama = 5,
+        Espionagem = 6,
+        Faroeste = 8,
+        Ficcao_Cientifica = 9,
+        Romance = 10
+
+    }
+}
